fe3bcdaf8c0bd782b06c6618363b8c4fe24584d9
rbansal2453 <2453rajesh@gmail.com>
2017-03-24 15:48:19 -0500

    API

---
+++ DealerVehicle/WebAPI/ModelsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DealerVehicle;
+using DealerVehicle.Models;
+using Newtonsoft.Json;
+
+namespace DealerVehicle.WebAPI
+{
+    public class ModelsController : ApiController
+    {
+        private Repository.ModelRepo db = new Repository.ModelRepo();
+
+        // GET: api/Models
+        [ResponseType(typeof(List<Model>))]
+        public IHttpActionResult GetModels()
+        {
+            var result = JsonConvert.SerializeObject(db.GetModelsAll(), new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+            return Ok(result);
+        }
+
+        // GET: api/Models/5
+        [ResponseType(typeof(Model))]
+        public IHttpActionResult GetModel(int id)
+        {
+            Model model = db.GetModelById(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        // PUT: api/Models/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutModel(int id, Model model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != model.ModelId)
+            {
+                return BadRequest();
+            }
+
+            db.InsertModel(model);
+
+            try
+            {
+                //db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Models
+        [ResponseType(typeof(Model))]
+        public IHttpActionResult PostModel(Model model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+           db.InsertModel(model);
+            return CreatedAtRoute("DefaultApi", new { id = model.ModelId }, model);
+        }
+
+        // DELETE: api/Models/5
+        [ResponseType(typeof(Model))]
+        public IHttpActionResult DeleteModel(int id)
+        {
+            Model model = db.GetModelById(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            db.DeleteModel(model);
+
+            return Ok(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ModelExists(int id)
+        {
+            return db.GetModelsAll().Count(e => e.ModelId == id) > 0;
+        }
+    }
+}
